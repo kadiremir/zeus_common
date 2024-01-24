faaf4c83c6e78c11ef23b7f0dcd43602e93dcaf0
Kadir Emir <92856123+kadiremir@users.noreply.github.com>
2024-01-24 12:28:43 +0100

    Delete trial.ps1

--- trial.ps1
+++
@@ -1,5 +0,0 @@
-(""|Out-String).Trim()|Out-File -encoding ASCII new_env_var.properties
-
-$wheelPath = "C:"
-
-("ZEUS_CORE_BRANCH_WHEEL_DIR=$wheelPath"|Out-String).Trim()|Out-File -encoding ASCII new_env_var.properties
